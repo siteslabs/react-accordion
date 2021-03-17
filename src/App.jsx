ae4a1d81b6a099e6fc9d13430c00109fd2548ecf
beksultan <beksultan.mamashov@gmail.com>
2021-03-17 10:06:15 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from "react"
+import data from "./data"
+import SingleQuestion from "./Question"
+function App() {
+  const [questions, setQuestions] = useState(data)
+  return (
+    <main>
+      <div className="container">
+        <h3>question and answers about login</h3>
+        <section className="info">
+          {questions.map((q) => {
+            return <SingleQuestion key={q.id} {...q} />
+          })}
+        </section>
+      </div>
+    </main>
+  )
+}
+
+export default App
